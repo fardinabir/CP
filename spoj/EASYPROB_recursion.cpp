04cb4594a664427e2c97a3ffb1bf980022546d0b
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2019-09-14 00:45:13 +0600

    changes

--- spoj/EASYPROB_recursion.cpp
+++ spoj/EASYPROB_recursion.cpp
@@ -1,3 +1,5 @@
+///spoj easyprob - recursion
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,7 +12,7 @@ void call(int n)
         printf("0");
         return;
     }
-    while(log2(n))
+    while(n>0)
     {
         printf("2");
         x=log2(n);
@@ -20,8 +22,6 @@ void call(int n)
             call(x);
             printf(")");
         }
-        else
-            printf("2");
         n-=(round(pow(2,x)));
         if(n>0)
             printf("+");
@@ -31,6 +31,12 @@ void call(int n)
 int main()
 {
     int n,i,j,k,l,m,s=0;
-    cin>>n;
-    call(n);
+    int arr[7]={137, 1315, 73, 136, 255, 1384, 16385};
+    for(i=0;i<7;i++)
+    {
+        cout<<arr[i]<<"=";
+        call(arr[i]);
+        cout<<endl;
+    }
+    return 0;
 }
