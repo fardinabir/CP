51ae003b5ba1164465b2caf571d3aa1a274b59cf
fardinabir <fardinabir@gmail.com>
2021-03-23 11:20:32 +0600

    changes

--- temp1/main.cpp
+++ temp1/main.cpp
@@ -1,22 +1,17 @@
-    #include <bits/stdc++.h>
-    #define mx 100000
-    #define ll long long int
-    #define test_case printf("Case %d:\n",id++);
+#include <bits/stdc++.h>
+#define mx 100000
+#define ll long long int
+#define test_case printf("Case %d:\n",id++);
 
-    using namespace std;
+using namespace std;
 
-    int a[1000];
+ll arr[200005],n;
 
-    int main()
-    {
-        ll t,i,j,k,l,b,c=0,p,d=1,e,u,v,q,sum,id=1,x,y;
-        cin>>t;
-        while(t--)
-        {
-            cin>>a[0]>>a[1]>>a[2]>>a[3];
-            cout<<a[1]<<" "<<a[2]<<" "<<a[2]<<endl;
-        }
-
-
-        return 0;
-    }
+int main()
+{
+    //ll t,i,j,k,l,a,b,c=0,p,d=1,e,u,v,q,sum,id=1,x,y;
+    int k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    cout<<min(c*d,min(k*l/nl,p/np))/n;
+    return 0;
+}
